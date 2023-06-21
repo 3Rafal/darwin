ee2f06e011071c16ce2923716ce30947ccd399a2
Rafal Gwozdzinski <rafal@gwozdzinski.com>
2023-06-21 13:58:50 +0200

    Modest beginning

---
+++ darwin-configuration.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages =
+    [ pkgs.vim
+      pkgs.pandoc
+      pkgs.rectangle
+
+      # dev
+      pkgs.silver-searcher
+      pkgs.coreutils
+      pkgs.jq
+      pkgs.llvm
+      pkgs.pkg-config
+      pkgs.pcre
+      pkgs.direnv
+
+      # ocaml
+      pkgs.opam
+    ];
+  
+  homebrew.enable = true;
+  homebrew.brews =
+  [
+    {
+    name = "d12frosted/emacs-plus/emacs-plus";
+    args = [ 
+      "with-no-titlebar" 
+      "with-native-comp"
+      "with-retro-emacs-logo-icon"
+      ];
+    }
+  ];
+
+  services.nix-daemon.enable = true;
+
+  programs.zsh.enable = true;
+
+  system.stateVersion = 4;
+}
